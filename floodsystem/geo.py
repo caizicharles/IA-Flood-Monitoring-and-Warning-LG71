37676178d25c33d8d71d0fbb8c5c3263e6c38c43
caizicharles <zc348@cam.ac.uk>
2022-01-25 23:38:39 +0000

    add

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,22 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+def stations_by_distance(stations, p):
+
+    name_n_dis = []
+    s_name = []
+    s_dis = []
+
+    for location in stations:
+        if location.name is True:
+            s_name.append(location.name)
+        
+        elif location.coord is True:
+            s_dis.append(haversine(location.coord, p))
+
+    for i in s_name, s_dis:
+        name_n_dis.append(tuple(s_name[i], s_dis[i]))
+
+    return name_n_dis
