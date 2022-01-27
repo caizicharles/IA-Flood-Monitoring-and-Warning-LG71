5adf4f7f8acb3c610117c5a67fac4ba9bad99da2
caizicharles <zc348@cam.ac.uk>
2022-01-27 10:49:38 +0000

    add

--- Task1B.py
+++ Task1B.py
@@ -1,3 +1,15 @@
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
 
-        
+def show():
+
+    show_list0 = []
+    show_list = []
+    show_list0.append(stations_by_distance(build_station_list(), (1, 2)))
+
+    for j in range(0, 10):
+        show_list.append(show_list0[j])
+
+    return show_list
+
+print(show())
