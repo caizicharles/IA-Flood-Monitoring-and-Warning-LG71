85122accff8fbddf71c94b991202427f26ad5f3e
caizicharles <zc348@cam.ac.uk>
2022-01-30 23:20:19 +0000

    add

--- Task1E.py
+++ Task1E.py
@@ -1,20 +1,18 @@
 from floodsystem.stationdata import build_station_list
 
+print('-----------------------')
 
 def run():
     """Requirements for Task 1A"""
 
     # Build list of stations
     stations = build_station_list()
+    rivers = set()
+    for station in stations:
+        rivers.add(station.river)
 
-    # Print number of stations
-    print("Number of stations: {}".format(len(stations)))
-
-    # Display data from 3 stations:
-    for station in range(50):
-        print(stations[station])
+    river = sorted(rivers)
+    return river
 
 
-if __name__ == "__main__":
-    print("*** Task 1A: CUED Part IA Flood Warning System ***")
-    run()
+print(run())
